983692dea19d7891adaef640e2563d579e4a06c8
ProjectRobal <patryk1-70@wp.pl>
2025-06-02 19:52:25 +0200

    Now compose should check for nvidia or amd gpu

--- run.sh
+++ run.sh
@@ -4,4 +4,12 @@ clear
 
 xhost local:docker
 
-docker compose up
+if lshw -C display 2>/dev/null | grep -i "vendor" | grep -qi "nvidia"; then
+    echo "NVIDIA is present as a display vendor."
+
+    docker compose -f compose_nvidia.yaml up
+else
+
+    docker compose -f compose_amd.yaml up
+fi
+
