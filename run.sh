7688b8c2c8c004c547936456f0b4f14d4bb2f4f0
ProjectRobal <patryk1-70@wp.pl>
2025-10-20 18:24:56 +0200

    run.sh: Added option to run middleware and build container, some refactoring.

--- run.sh
+++ run.sh
@@ -7,9 +7,24 @@ xhost local:docker
 if lshw -C display 2>/dev/null | grep -i "vendor" | grep -qi "nvidia"; then
     echo "NVIDIA is present as a display vendor."
 
-    docker compose -f compose_nvidia.yaml up
+    COMPOSE_FILE=compose_nvidia.yaml
+
 else
 
-    docker compose -f compose_amd.yaml up
+    COMPOSE_FILE=compose_amd.yaml
+
 fi
 
+if [[ $1 == "pilot" ]]; then
+
+    docker compose -f $COMPOSE_FILE exec px4_sim MicroXRCEAgent udp4 -p 8888
+
+elif [[ $1 == "build" ]]; then
+
+    docker compose -f $COMPOSE_FILE build
+
+else
+
+    docker compose -f $COMPOSE_FILE up
+
+fi
