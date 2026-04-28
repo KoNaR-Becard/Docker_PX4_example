0d61a43e6437f0d72da5e8da1b8e2e48c86c2403
Marcin Gorniak <marcingorniak23@gmail.com>
2026-04-28 23:04:44 +0200

    Testing on Intel Camera

--- ai_logic/src_ai/scripts/detect_live.py
+++ ai_logic/src_ai/scripts/detect_live.py
@@ -1,7 +1,18 @@
 import cv2
+import time
 from ultralytics import YOLO
 
 MODEL_PATH = "/home/marcin-g-rniak/Docker_PX4_example/runs/obb/train2/weights/best.pt" # dopasować ścieżkę do swojej
+CAMERA_PORT = 37
+
+def send_to_pixhawk(see_panel, x=0.0, y=0.0, obrot=0.0, status="SZUKAM"):
+    #print(f"Wysyłam -> Panel: {see_panel} | X:{x:.1f} Y:{y:.1f} Angle:{obrot:.2f} | Status:{status}")
+    pass
+
+def collect_command_from_pixhawk(buttom):
+    if buttom == ord('p'):
+        return "Zrobione"
+    return "Brak"
 
 def main():
     print(f"Ładowanie wyuczonego modelu z: {MODEL_PATH}")
@@ -11,7 +22,7 @@ def main():
         print(f"Błąd! Nie znaleziono modelu. Sprawdź ścieżkę. Szczegóły: {e}")
         return
 
-    cap = cv2.VideoCapture(32, cv2.CAP_V4L2) # dopasować do kamerki z drona(port 36/34?)
+    cap = cv2.VideoCapture(CAMERA_PORT, cv2.CAP_V4L2)
     
     if not cap.isOpened():
         print("Błąd: Nie można otworzyć kamery w laptopie.")
@@ -27,12 +38,42 @@ def main():
             break
 
         results = model.predict(frame, conf=0.5, verbose=False)
-        
-        annotated_frame = results[0].plot()
+        buttom = cv2.waitKey(1) & 0xFF
 
+        if results[0].obb is not None and len(results[0].obb) > 0:
+            print("Wykryto panel")
+            
+            data = results[0].obb.xywhr[0] 
+            x_center= float(data[0])
+            y_center = float(data[1])
+            width = float(data[2])
+            height = float(data[3])
+            rotation_angle = float(data[4])
+
+            send_to_pixhawk(see_panel=True, x=x_center, y=y_center, obrot=rotation_angle)
+
+            command = collect_command_from_pixhawk(buttom)
+            
+            if command == "Zrobione":
+                file_name = f"panel_analiza_{int(time.time())}.jpg"
+                
+                cv2.imwrite(file_name, frame)
+                print(f"Zdjęcie zapisane jako: {file_name}")
+                
+                send_to_pixhawk(see_panel=True, status="Zrobione")
+                
+                time.sleep(1) 
+
+        else:
+            send_to_pixhawk(see_panel=False, status="Clean")
+
+        annotated_frame = results[0].plot()
+        if results[0].obb is not None and len(results[0].obb) > 0:
+             cv2.drawMarker(annotated_frame, (int(x_center), int(y_center)), (0, 0, 255), 
+                            markerType=cv2.MARKER_CROSS, markerSize=20, thickness=2)
         cv2.imshow("Wykrywanie Puszki na Zywo (OBB)", annotated_frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if buttom == ord('q'):
             break
 
     cap.release()
